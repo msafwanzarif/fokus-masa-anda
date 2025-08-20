7ad07ecd78760d7b469e68df544c34fd315f9f14
Safwan Zarif <msafwanzarif@gmail.com>
2025-08-20 23:05:15 +0800

    remove unnecessary i18n module

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Translation from '../views/Translation.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/translation',
-    name: 'translate',
-    component: Translation
   },
 ]
 
