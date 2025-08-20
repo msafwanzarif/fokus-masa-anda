4d14916ccd61233486bdb3ed9bf5a4b2f19fac52
Safwan Zarif <msafwanzarif@gmail.com>
2025-08-20 23:20:39 +0800

    add pwa after ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,43 @@ export default defineConfig({
       propsDestructure: true
     }
   }),
+    VitePWA({
+        registerType: 'autoUpdate',  // updates SW in background
+        manifest: {
+          name: 'Fokus ! Manfaatkan Masa Anda',
+          short_name: 'Fokus',
+          start_url: '.',
+          display: 'standalone',
+          background_color: '#ffffff',
+          theme_color: '#42b883',
+          icons: [
+            {
+              src: '/web-app-manifest-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/web-app-manifest-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ],
+          "screenshots": [
+            {
+              "src": "/screenshot-wide.png",
+              "sizes": "1280x720",
+              "type": "image/png",
+              "form_factor": "wide"
+            },
+            {
+              "src": "/screenshot-mobile.png",
+              "sizes": "375x667",
+              "type": "image/png",
+              "form_factor": "narrow"
+            }
+          ]
+        }
+      })
   ],
   resolve: {
     alias: {
