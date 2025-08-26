58ff6e96cf705a0bb556fe1782b762548b3c712c
Safwan Zarif <msafwanzarif@gmail.com>
2025-08-26 10:14:33 +0800

    feat: Update idle page Design

--- src/views/IdleView.vue
+++ src/views/IdleView.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>
-    You are idle / Logged in elsewhere.
+  <div class="container h-100 w-100">
+    <div class="d-flex flex-column justify-content-center align-items-center h-100">
+      <p class="text-center px-5">Anda dah lama tidak aktif atau anda sedang guna di tempat lain</p>
+      <button class="btn btn-primary" @click="useHere()">
+        Gunakan di Sini
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
+
+function useHere(){
+  router.push({ name: 'home' });
+}
 </script>
